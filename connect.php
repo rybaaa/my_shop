19fe1c2052fce3e852b087ce2aecac95e97bd63b
rybaaa <bolshayaryba22@gmail.com>
2023-05-12 22:14:11 +0200

    initialization

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+
+$connection = new PDO('mysql:host=localhost;dbname=shop', 'root', 'mysql');
+
+
+
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div>
+        <a href="add_product.php" class="logo">LOGO</a>
+        <nav>
+            <a href="add_product.php">ADD PRODUCT</a>
+            <a href="all_products.php">ALL PRODUCTS</a>
+            <a href="my_cart.php">MY CART</a>
+        </nav>
+    </div>
+</header>
